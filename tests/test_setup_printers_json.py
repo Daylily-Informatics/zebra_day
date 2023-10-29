3fb84f52bf60a9b42d1ffd65d4196a2e5396c07a
John Major <daylily@daylilyinformatics.com>
2023-10-29 07:31:34 -0700

    8 tests all passing

--- tests/test_setup_printers_json.py
+++ tests/test_setup_printers_json.py
@@ -4,23 +4,17 @@ import random
 def test_printers_config_loading():
     
     from zebra_day import print_mgr as zd
-
     zd_pm = zd.zpl()
-
     assert 'labs' in zd_pm.printers.keys()
 
 
 def test_printers_clear_reset():
 
-
     from zebra_day import print_mgr as zd
-
     zd_pm = zd.zpl()
-
     zd_pm.clear_printers_json()
 
     assert ('labs' in zd_pm.printers.keys() and len(zd_pm.printers['labs'].keys()) == 0)
-
 
     zd_pm.create_new_printers_json_with_single_test_printer()
     assert zd_pm.printers  == {'labs': {'scan-results': {'Download-Label-png': {'ip_address': 'dl_png',
@@ -30,17 +24,23 @@ def test_printers_clear_reset():
                                                                                 'serial': 'na',
                                                                                 'arp_data': ''}}}}
 
+
+def test_manipulating_printers_json():
+    
+    from zebra_day import print_mgr as zd
+    zd_pm = zd.zpl()
+    zd_pm.clear_printers_json()
+    
     zd_pm.printers['labs']['test'] = {}
+
     tmp_json = f"etc/tmp_printers{random.randint(0,1000)}.json"
     
     zd_pm.save_printer_json(tmp_json)
+
     assert 'test' in zd_pm.printers['labs'].keys()
-    
     assert zd_pm.printers_filename.removesuffix(tmp_json)
     assert os.path.exists(zd_pm.printers_filename)
-    
-    print("x",zd_pm.printers_filename)
-    
+        
     zd_pm.clear_printers_json(tmp_json)
     assert 'test' not in zd_pm.printers['labs'].keys()
     
@@ -49,13 +49,10 @@ def test_printers_clear_reset():
     zd_pm.printers['labs']['test'] = {}
     zd_pm.save_printer_json(tmp_json)
 
-
     assert zd_pm.printers['labs']['test'] == {}
-
 
     zd_pm.create_new_printers_json_with_single_test_printer()
     
-
     assert zd_pm.printers == {'labs': {'scan-results': {'Download-Label-png': {'ip_address': 'dl_png',
                                                                                'label_zpl_styles': ['test_2inX1in'],
                                                                                'print_method': 'generate png',
@@ -64,22 +61,53 @@ def test_printers_clear_reset():
                                                                                'arp_data': ''}}}}
 
 
-    
+def test_creating_zpl_from_template():
+
+    from zebra_day import print_mgr as zd
+    zd_pm = zd.zpl()
+    zd_pm.clear_printers_json()
+
     zpl = zd_pm.formulate_zpl(uid_barcode='TESTBC', alt_a='A', alt_b='B', alt_c='C', alt_d='D', alt_e='E', alt_f='F', label_zpl_style='test_2inX1in')
     assert zpl == """^XA\n^FO235,20\n^BY1\n^B3N,N,40,N,N\n^FDTESTBC^FS\n^FO235,70\n^ADN,30,20\n^FDTESTBC^FS\n^FO235,115\n^ADN,25,12\n^FDalt_a^FS\n^FO235,145\n^ADN,25,12\n^FDalt_b^FS\n^FO70,180\n^FO235,170\n^ADN,30,20\n^FDalt_c^FS\n^FO490,180\n^ADN,25,12\n^FDalt_d^FS\n^XZ"""
 
-    tmp_png = f"files/test_png_{random.randint(0,101010)}.png"
-    zd_pm.generate_label_png(zpl_string=zpl, png_fn=tmp_png)
-    assert os.path.exists(tmp_png)
+    
+def test_creating_a_png_of_label_zpl():
 
-    zd_pm.clear_printers_json(tmp_json)
+
+    from zebra_day import print_mgr as zd
+    zd_pm = zd.zpl()
+    zd_pm.clear_printers_json()
+    zpl = zd_pm.formulate_zpl(uid_barcode='TESTBC', alt_a='A', alt_b='B', alt_c='C', alt_d='D', alt_e='E', alt_f='F', label_zpl_style='test_2inX1in')    
+    tmp_png = f"{os.path.dirname(zd.__file__)}/files/test_png_{random.randint(0,101010)}.png"
+    tmp_full_png = zd_pm.generate_label_png(zpl_string=zpl, png_fn=tmp_png, relative=False)
+
+    assert os.path.abspath(tmp_png) == os.path.abspath(tmp_full_png)
+
+
+def test_printing_label():
+    # Also prints a png, but uses the label printing path to do so.
+    
+    from zebra_day import print_mgr as zd
+    zd_pm = zd.zpl()
+    zd_pm.clear_printers_json()
+    
     zd_pm.create_new_printers_json_with_single_test_printer()
-    tmp_png_2 =  zd_pm.print_zpl(lab="scan-results", printer_name="Download-Label-png", uid_barcode='TESTBC', alt_a='A', alt_b='B', alt_c='C', alt_d='D', alt_e='E', alt_f='F', label_zpl_style="test_2inX1in", client_ip='pkg', print_n=1, zpl_content=None, relative=True)
+    tmp_png_2 =  zd_pm.print_zpl(lab="scan-results", printer_name="Download-Label-png", uid_barcode='TESTBC', alt_a='A', alt_b='B', alt_c='C', alt_d='D', alt_e='E', alt_f='F', label_zpl_style="test_2inX1in", client_ip='pkg', print_n=1, zpl_content=None)
 
     assert os.path.exists(tmp_png_2)
     
 
-    probe_zebra_printers_add_to_printers_json(self, ip_stub="192.168.1", scan_wait="0.25",lab="unit_test"):
+def test_probing_network_for_printers():
+    # Expected to fail, testing if the top level png printer is added
+
+    
+    from zebra_day import print_mgr as zd
+    zd_pm = zd.zpl()
+    zd_pm.clear_printers_json()
+    zd_pm.create_new_printers_json_with_single_test_printer()
+
+    # Set the scan wait to FAR too short.  Good values are like 0.5+
+    zd_pm.probe_zebra_printers_add_to_printers_json(ip_stub="192.168.1", scan_wait="0.01",lab="unit_test")
 
     # this just sees if the mechanise work... can't scan the network.
     assert 'unit_test' in zd_pm.printers['labs']
