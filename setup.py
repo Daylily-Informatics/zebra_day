e33d2220168aebd40a1cde05ec7b275a12ae43fa
John Major <daylily@daylilyinformatics.com>
2023-10-29 11:14:05 -0700

    v0.2.1

--- setup.py
+++ setup.py
@@ -2,19 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='zebra_day',
-    version='0.2.0',
+    version='0.2.1',
     description='A Python library to manage a zebra printer fleet and an api for ZPL print requests.',
     author='John Major',
     author_email='john@daylilyinformatics.com',
     url='https://github.com/Daylily-Informatics/zebra_day',
     packages=find_packages(),
-    install_requires=[
-        'yaml_config_day',
-        'requests',
-        'pytz',
-        'cherrypy',
-        'ipython'
-    ],
+    install_requires=["yaml_config_day==0.0.5", "requests", "pytz==2023.3.post1", "cherrypy==18.8.0", "ipython==8.16.1", "pytest"],
     include_package_data=True,
     package_data={
         'zebra_day': [
